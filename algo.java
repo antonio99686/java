674330e03f9e868d58226fddffa11ab0bddbe197
antonio99686 <antoniomongelo72@gmail.com>
2026-03-16 21:33:07 -0300

    algo

---
+++ algo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class algo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("digite um numero:");
+        int n = scanner.nextInt();
+        System.out.println("voce entrou: " + n);
+    }
+}
